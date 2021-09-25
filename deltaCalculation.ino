7a17bcf646397e9f1904d13b907893dcc3929d1b
tariqaziz9164 <81738455+tariqaziz9164@users.noreply.github.com>
2021-09-25 14:02:14 +0500

    Add files via upload
    Add a delta finding parogram file

---
+++ deltaCalculation.ino
@@ -0,0 +1,32 @@
+float Vsolar;
+float Isolar;
+float Psolar;
+float PsolarPrev;
+float deltaPsolar = Psolar - PsolarPrev;
+float deltaPwm = pwm - pwmOld;
+float pwmOld ;
+float pwm;
+void loop()
+           {
+         if  ( deltaPsolar >= 0)
+                               {
+                                if(deltaPwm >= 0)
+                                                 {
+                                                  pwm = pwm + deltaPwm;
+                                                 }
+                                                else {
+                                                     pwm = pwm - deltaPwm;
+                                                     }
+                               }
+
+                    if(deltapwm >= 0)
+                                     {
+                                      pwm = pwm - deltaPwm
+                                     }
+
+                                 pwm = pwm + deltaPwm;    
+           }
+           PsolarPrev = Psolar;
+           pwmOld = pwm;
+
+
